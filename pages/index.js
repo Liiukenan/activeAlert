bcf37afabaaebb500e12bd874717ec79eda49a5d
liukenan <liukenan@meetstarinc.com>
2020-11-23 11:14:42 +0800

    安装lottie插件

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
+import React,{useState} from 'react'
 import Head from 'next/head'
 import "./css/home.styl"
-
-
+import Lottie from 'react-lottie'
+// import * as animationData from './pinjump.json'
 function Home() {
- 
   return (
     <div className="home">
 
